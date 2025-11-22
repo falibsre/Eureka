a26743ef103d3a7e361a8b947510ee0f994ef13a
fali <baszucki906@gmail.com>
2025-11-22 17:47:16 +0300

    22 November 2025

--- Docs.c
+++ Docs.c
@@ -1,5 +1,7 @@
 #include "Eureka.c"
 int main() {
+    int result = square(5);
     print("Hello!"); // Hello!
+    print("\nSquare 5: %d", result); // Square 5: 25
     waen(); // Press enter to continue...
 }
--- Eureka.c
+++ Eureka.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-void print(const char* text) {
-  printf("%s", text);
+#include <stdarg.h>
+void print(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
 }
 void waen() {
-  printf("\nPress enter to continue...");
+  printf("\nPress enter to continue...\n");
   getchar();
+}
+int square(int number) {
+  return number * number;
 }
