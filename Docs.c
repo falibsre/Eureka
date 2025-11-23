034a31947daedc00fcf4574ecd384007cdb4f71f
fali <baszucki906@gmail.com>
2025-11-23 15:30:49 +0300

    Delete Docs.c

--- Docs.c
+++
@@ -1,7 +0,0 @@
-#include "Eureka.c"
-int main() {
-    int result = square(5);
-    print("Hello!"); // Hello!
-    print("\nSquare 5: %d", result); // Square 5: 25
-    waen(); // Press enter to continue...
-}
