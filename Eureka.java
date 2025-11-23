c8d464ceff968ee4c7bbb71f6f4418236ccb6ef3
fali <baszucki906@gmail.com>
2025-11-23 15:31:12 +0300

    23 November 2025

---
+++ Eureka.java
@@ -0,0 +1,11 @@
+public class Eureka {
+    public static void print(String text) {
+        System.out.println(text);
+    }
+    public static int square(int number) {
+        return number * number;
+    }
+    public static int area(int number1, int number2) {
+        return number1 * number2;
+    }
+}
